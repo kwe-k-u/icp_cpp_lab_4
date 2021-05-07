1043c6cf2fe9309b3cbf11124378197a2b87cdd9
MateenAndan <77847288+MateenAndan@users.noreply.github.com>
2021-05-07 19:54:00 +0000

    Add files via upload

---
+++ cylindricalBlocks.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef CYLINDRICALBLOCKS_H
+#define CYLINDRICALBLOCKS_H
+
+class cylindricalBlocks : public sqrBaseRectBlocks{
+public:
+	cylindricalBlocks();
+	cylindricalBlocks(int height, int diameter);
+};
+
+
+#endif
+
