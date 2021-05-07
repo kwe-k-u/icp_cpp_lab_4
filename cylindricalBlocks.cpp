6e3cc031bec60896c22aaa05cbde35cdc635359a
MateenAndan <77847288+MateenAndan@users.noreply.github.com>
2021-05-07 19:53:25 +0000

    Update cylindricalBlocks.cpp

--- cylindricalBlocks.cpp
+++ cylindricalBlocks.cpp
@@ -1,1 +1,13 @@
+#include<iostream>
+#include "sqrBaseRectBlocks.h"
+#include "cylindricalBlocks.h"
+using namespace std;
+
+cylindricalBlocks::cylindricalBlocks()
+	: sqrBaseRectBlocks{} {}
+
+
+cylindricalBlocks::cylindricalBlocks(int height,int diameter)
+	: sqrBaseRectBlocks{ height, diameter } {}
+
 
